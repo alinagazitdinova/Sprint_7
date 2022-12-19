565ac5ccc47d00e894be3cb875ad485eba0cec4d
alinagazitdinova <alina.gazitdinova@yandex.ru>
2022-12-19 17:06:32 +0300

    Добавлен класс CreateOrderTest

---
+++ src/test/java/sprint7/OrderCreation/CreateOrderTest.java
@@ -0,0 +1,39 @@
+package sprint7.OrderCreation;
+
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import sprint7.Helpers.OrderNoColor;
+
+import static io.restassured.RestAssured.given;
+
+
+public class CreateOrderTest {
+    public OrderNoColor orderNoColor;
+    public String color = null;
+
+    @Before
+    public void setUp() {
+
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+        orderNoColor = new OrderNoColor("alina", "Blinchik", "Lenina, 8", "6", "+7 800 355 35 35", "3",  "2022-12-31", "the fastest, please", color);
+    }
+
+
+    @Test
+    public void orderCreation() { // этом тесте создаем заказ без указания цвета , проверяем, что в ответе есть track
+
+        int created = given().log().all()
+                .header("Content-Type", "application/json")
+                .body(orderNoColor)
+                .when()
+                .post("/api/v1/orders")
+                .then().log().all()
+                .statusCode(201)
+                .extract()
+                .path("track")
+                                           ;
+
+        assert created != 0;
+    }
+}
