37c082b3365ec32b62cf1a8310a3fef0d5181439
alinagazitdinova <alina.gazitdinova@yandex.ru>
2022-12-19 17:04:41 +0300

    Добавлен класс CourierGenerator

---
+++ src/test/java/sprint7/Helpers/CourierGenerator.java
@@ -0,0 +1,15 @@
+package sprint7.Helpers;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class CourierGenerator {
+
+    public Courier random(){
+        return new Courier(RandomStringUtils.randomAlphanumeric(10), "1234512345", "blinchik");
+
+    }
+
+    public Courier generic() {
+        return new Courier("alina", "1234512345", "blinchik");
+    }
+}
