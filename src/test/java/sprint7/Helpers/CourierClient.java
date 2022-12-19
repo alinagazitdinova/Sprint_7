9ce04567b8d658d3abcc0145199b7b9f738f5b24
alinagazitdinova <alina.gazitdinova@yandex.ru>
2022-12-19 17:04:04 +0300

    Добавлен класс Courier

---
+++ src/test/java/sprint7/Helpers/CourierClient.java
@@ -0,0 +1,46 @@
+package sprint7.Helpers;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
+
+
+public class CourierClient {
+    private final String ROOT = "/api/v1/courier";
+
+
+    public ValidatableResponse create(Courier courier){
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(courier)
+                .when()
+                .post(ROOT)
+                .then().log().all();
+
+    }
+    public ValidatableResponse login(Credentials creds){
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .and()
+                .body(creds)
+                .when()
+                .post(ROOT +"/login")
+                .then().log().all();
+
+    }
+
+
+    public ValidatableResponse delete(int courierId) {
+        String json = String.format("{\"id\":\"%d\"}", courierId);
+       return given().log().all()
+
+                .body(json)
+                .when()
+                .delete(ROOT + "/" + courierId)
+                .then().log().all();
+
+
+    }
+}
